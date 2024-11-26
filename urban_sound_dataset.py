1f49d2b2af5b8667c4ac85062f783a83553a2c5f
Gennady <thegreatcombinaator@gmail.com>
2024-11-26 13:25:17 +0300

    add mel spectrograms

--- urban_sound_dataset.py
+++ urban_sound_dataset.py
@@ -1,13 +1,16 @@
 import os
 
+import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
 
 class UrbanSoundDataset(Dataset):
-    def __init__(self, annotations_file, audio_dir):
+    def __init__(self, annotations_file, audio_dir, transformation, target_sample_rate):
         self.annotations = pd.read_csv(annotations_file)
         self.audio_dir = audio_dir
+        self.transformation = transformation
+        self.target_sample_rate = target_sample_rate
 
     def __len__(self):
         return len(self.annotations)
@@ -16,7 +19,21 @@ class UrbanSoundDataset(Dataset):
         audio_sample_path =  self._get_audio_sample_path(index)
         label = self._get_audio_sample_label(index)
         signal, sr = torchaudio.load(audio_sample_path)
+        signal = self._resample_if_necessary(signal, sr)
+        signal = self._mix_down_if_necessary(signal)
+        signal = self.transformation(signal)
         return signal, label
+
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
+
+    def _mix_down_if_necessary(self, signal):
+        if signal.shape[0] > 1:
+            signal = torch.mean(signal, dim=0, keepdim=True)
+        return signal
 
     def _get_audio_sample_path(self, index):
         fold = f'fold{self.annotations.iloc[index, 5]}'
@@ -30,10 +47,17 @@ class UrbanSoundDataset(Dataset):
 if __name__ == '__main__':
     ANNOTATIONS_FILE = 'C:/Users/bemyp/PycharmProjects/add-laugh-tracks/data/UrbanSound8K/UrbanSound8K/metadata/UrbanSound8K.csv'
     AUDIO_DIR = 'C:/Users/bemyp/PycharmProjects/add-laugh-tracks/data/UrbanSound8K/UrbanSound8K/audio'
+    SAMPLE_RATE = 16000
 
-    usd = UrbanSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR)
+    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64
+    )
+
+    usd = UrbanSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE)
     print(f'There are {len(usd)} audio samples')
 
     signal, label = usd[0]
-
     print(label)
